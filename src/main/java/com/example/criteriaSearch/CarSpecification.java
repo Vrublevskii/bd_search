0587b85453c527dd3034b08656ee0eff81260ba0
Vrublevskii <vrubleuski.viktar@gmail.com>
2022-12-06 12:59:25 +0300

    bd теперь со связями

--- src/main/java/com/example/criteriaSearch/CarSpecification.java
+++ src/main/java/com/example/criteriaSearch/CarSpecification.java
@@ -16,23 +16,22 @@ public class CarSpecification implements Specification<Car> {
     public Predicate toPredicate(Root<Car> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
 
         if (searchCriteria.getValue().equals("")) {
-            if (searchCriteria.getKey().equals("name_driver") || searchCriteria.getKey().equals("last_name_driver")) {
-                return criteriaBuilder.isNull(driverJoin(root).get(searchCriteria.getKey()));
+            if (searchCriteria.getKey().equals("name_driver")
+                    || searchCriteria.getKey().equals("last_name_driver")
+                    || searchCriteria.getKey().equals("id_driver")
+            ) {
+                return criteriaBuilder.isNull(root.join("driver").get(searchCriteria.getKey()));
             }
             return criteriaBuilder.isNull(root.get(searchCriteria.getKey()));
         } else if (searchCriteria.getKey().equals("name_driver") || searchCriteria.getKey().equals("last_name_driver")) {
-            return criteriaBuilder.like(driverJoin(root).get(searchCriteria.getKey()), (String) searchCriteria.getValue());
+            return criteriaBuilder.like(root.join("driver").get(searchCriteria.getKey()), (String) searchCriteria.getValue());
         } else if (searchCriteria.getKey().equals("id_driver")) {
-            return criteriaBuilder.equal(driverJoin(root).get(searchCriteria.getKey()), searchCriteria.getValue());
+            return criteriaBuilder.equal(root.join("driver").get(searchCriteria.getKey()), searchCriteria.getValue());
         } else if (root.get(searchCriteria.getKey()).getJavaType() == String.class) {
             return criteriaBuilder.like(root.get(searchCriteria.getKey()), (String) searchCriteria.getValue());
         } else {
             return criteriaBuilder.equal(root.get(searchCriteria.getKey()), searchCriteria.getValue());
         }
-    }
-
-    private Join<Car, Driver> driverJoin(Root<Car> root) {
-        return root.join("driver");
     }
 
 }
