dc3b63a3378bb880c6e8ab3038137a36ea6e6a29
Vrublevskii <vrubleuski.viktar@gmail.com>
2022-12-06 13:14:44 +0300

    new DB

--- src/main/resources/sql_script/db_for_isd.sql
+++ src/main/resources/sql_script/db_for_isd.sql
@@ -1,27 +1,44 @@
 create schema db_for_isd;
 
-create table db_for_isd.car(
-	id int primary key auto_increment,
-    name varchar (20) not null,
-    color varchar(20),
-    year int
+create table db_for_isd.driver(
+	id_driver int primary key auto_increment,
+    name_driver varchar (20) not null,
+    last_name_driver varchar (20)
 );
 
-insert into db_for_isd.car (name, color, year) values ('car1', 'red', 2000);
-insert into db_for_isd.car (name, color, year) values ('car1', 'blue', 2013);
-insert into db_for_isd.car (name, color, year) values ('car1', 'orange', 2017);
-insert into db_for_isd.car (name, year) values ('car2', 2020);
-insert into db_for_isd.car (name, color, year) values ('car3', 'blue', 1991);
-insert into db_for_isd.car (name, color, year) values ('car4', 'orange', 1987);
-insert into db_for_isd.car (name, color, year) values ('car4', 'lightblue', 1957);
-insert into db_for_isd.car (name, color, year) values ('car4', 'blue', 1999);
-insert into db_for_isd.car (name, color, year) values ('car5', 'red', 2002);
-insert into db_for_isd.car (name, color, year) values ('car6', 'blue', 2016);
-insert into db_for_isd.car (name, year) values ('car7', 2022);
-insert into db_for_isd.car (name) value ('car7');
-insert into db_for_isd.car (name) value ('car8');
-insert into db_for_isd.car (name, color) values ('car9', 'blue');
-insert into db_for_isd.car (name, color, year) values ('car10', 'yelow', 2001);
-insert into db_for_isd.car (name, color, year) values ('car11', 'lightblue', 1998);
+insert into db_for_isd.driver (name_driver, last_name_driver) values ('Driver', 'Last');
+insert into db_for_isd.driver (name_driver, last_name_driver) values ('Driver1', 'Last1');
+insert into db_for_isd.driver (name_driver, last_name_driver) values ('Driver2', 'Last1');
+insert into db_for_isd.driver (name_driver) values ('Driver3');
+insert into db_for_isd.driver (name_driver, last_name_driver) values ('Driver4', 'Last3');
+insert into db_for_isd.driver (name_driver, last_name_driver) values ('Driver4', 'Last4');
+
+select * from db_for_isd.driver;
+
+create table db_for_isd.car(
+	id_car int primary key auto_increment,
+    name varchar (20) not null,
+    color varchar(20),
+    year int,
+    id_driver_fk int,
+    constraint id_car_fk foreign key (id_driver_fk) references db_for_isd.driver (id_driver)
+);
+
+insert into db_for_isd.car (name, color, year, id_driver_fk) values ('car1', 'red', 2000, 1);
+insert into db_for_isd.car (name, color, year, id_driver_fk) values ('car1', 'blue', 2013, 1);
+insert into db_for_isd.car (name, color, year, id_driver_fk) values ('car1', 'orange', 2017, 1);
+insert into db_for_isd.car (name, year, id_driver_fk) values ('car2', 2020, 2);
+insert into db_for_isd.car (name, color, year, id_driver_fk) values ('car3', 'blue', 1991, 2);
+insert into db_for_isd.car (name, color, year, id_driver_fk) values ('car4', 'orange', 1987, 3);
+insert into db_for_isd.car (name, color, year, id_driver_fk) values ('car4', 'lightblue', 1957, 3);
+insert into db_for_isd.car (name, color, year, id_driver_fk) values ('car4', 'blue', 1999, 3);
+insert into db_for_isd.car (name, color, year, id_driver_fk) values ('car5', 'red', 2002, 4);
+insert into db_for_isd.car (name, color, year, id_driver_fk) values ('car6', 'blue', 2016, 4);
+insert into db_for_isd.car (name, year, id_driver_fk) values ('car7', 2022, 5);
+insert into db_for_isd.car (name, id_driver_fk) value ('car7', 5);
+insert into db_for_isd.car (name, id_driver_fk) value ('car8', 5);
+insert into db_for_isd.car (name, color, id_driver_fk) values ('car9', 'blue', 5);
+insert into db_for_isd.car (name, color, year, id_driver_fk) values ('car10', 'yellow', 2001, 6);
+insert into db_for_isd.car (name, color, year, id_driver_fk) values ('car11', 'lightblue', 1998, 6);
 
 select * from db_for_isd.car;
